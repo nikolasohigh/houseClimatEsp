ec34f083954051abcc5249a4ea1cfce5ba9b0222
nikolasohigh <kolya.bondar.yt@gmail.com>
2024-05-27 04:40:33 +0200

    final

--- include/constants.h
+++ include/constants.h
@@ -1,42 +1,20 @@
-/*ДЛЯ ДЕБАГА УБРАТЬ СЛЕДУЮЩИЙ ОДНОСТРОЧНЫЙ КОММЕНТ*/
-//#define BOT_DEBUG
-//#define MQTT_DEBUG
+#define THING_SPEAK_ADDRESS     ""         
+#define WRITE_API_KEY           ""  
+#define WIFI_SSID               ""               
+#define WIFI_PASSWORD           ""               
 
+#define RX_PIN                  14
+#define TX_PIN                  12 
 
-/*АВТОРИЗАЦИОННЫЕ ДАННЫЕ*/
-#define MQTT_ID                 "mqtt-kolya_bondar_yt-3f68k5"                       //Брать у dev.rightech.io
-#define WIFI_SSID               "ASOG BBW 56"                                       //SSID Вашей WI-FI сети
-#define WIFI_PASSWORD           "viA12439#56"                                       //Пароль Вашей WI-FI сети
-#define BOT_TOKEN               "6853517492:AAF0C3XLpF0qb-2vHPXHlgiEasl4dGBcoVg"    //Брать у https://t.me/BotFather
-#define ADMIN_ID                "6632566922"                                        //Брать у https://t.me/getmyid_bot. Можно вписать ID чата
+#define UTC                     1           
+#define UTCS                    1           
 
-/*ПИНЫ*/
-#define LED_PIN                 2           //Пин светодиода
+#define TIMEOUT                 5000        
 
-/*ЗАДЕРЖКИ (MS)*/
-#define BOT_MSG_DELAY           2000         //Задержка проверки сообщений (между ними)
-#define LED_DELAY               1000         //Задержка мигания светодиодом
-#define UPDATE_DELAY            2000         //Задержка опроса датчиков
-#define BOT_ALLERT_DELAY        3000         //Задержка проверки и отправка алертов
-#define REBOOT_DELAY            20000        //Задержка для перезагрузки. Если после перезагрузки через бота уходит в цикл - увеличиваем
-#define PUB_DELAY               10000        //Задержка публикации на MQTT-сервер
+#define NUM_FIELDS              4            
 
-/*ТОПИКИ*/
-#define TEMP_TOPIC              "temperature"    //Топик для температуры
-#define PPM_TOPIC               "ppm"            //Топик для ппм
-#define PRES_TOPIC              "pressure"       //Топик для давления
-#define HUM_TOPIC               "humidity"       //Топик для влажности
-
-/*НАСТРОЙКИ БОТА*/
-#define MAX_MESSAGE_LENGTH      100         //Максимальная длина сообщения
-
-/*КРИТИЧЕСКИЕ ЗНАЧЕНИЯ*/
-#define MAX_TEMP                24          //Максимальная температура, С
-#define MIN_TEMP                18          //Минимальная температура, С
-#define MAX_HYM                 60          //Максимальная влажность, %
-#define MIN_HYM                 30          //Минимальная влажность, %
-#define MAX_PPM                 1000        //Максимальная концентрация углекислого газа, ppm
-#define MAX_PRES                765         //Максимальное давление, ММРС
-#define MIN_PRES                755         //Минимальное давление, ММРС
-
+#define TEMPERATURE_FIELD       0           
+#define HUMIDITY_FIELD          1           
+#define PRESSURE_FIELD          2           
+#define PPM_FIELD               3           
     
--- src/MainDiplom.cpp
+++ src/MainDiplom.cpp
@@ -1,227 +1,224 @@
-#include <pgmspace.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>
-#include <EEPROM.h>
-#include "EspMQTTClient.h"
-#include "ArduinoOTA.h"
-#include "replies.h"
+#include <time.h>
+#include <ESP8266HTTPClient.h>
+#include "GyverTimer.h"
+#include <GyverOLED.h>
+#include <GyverBME280.h>
+#include <SoftwareSerial.h>
+#include "MHZ19.h"
 #include "constants.h"
 
-WiFiClientSecure client;
-X509List cert(TELEGRAM_CERTIFICATE_ROOT);
-UniversalTelegramBot bot(BOT_TOKEN, client);
-EspMQTTClient MQTTClient (WIFI_SSID, WIFI_PASSWORD, "dev.rightech.io", MQTT_ID);
 
-String temp, pres, hum, ppm;
-uint16_t bot_lastmsg, esp_lastled, mqtt_lastsend, esp_lastupdate;
-bool status;
+GyverBME280 bme;
+GyverOLED<SSD1306_128x64, OLED_BUFFER> screen;
+MHZ19 *mhz19_uart = new MHZ19(RX_PIN, TX_PIN);
 
-void handleNewMessages(int numNewMessages);
-void onConnectionEstablished(){}
+WiFiClient client;
+HTTPClient http;
 
+int humidity, temperature, pressure, ppm;
+
+GTimer sendDataTimer(MS);
+GTimer readSensorsTimer(MS);
+GTimer getTimeTimer(MS);
+
+
+String getResponse();
+void connectToWiFi();
+void HTTPPost(int numFields , String fieldData[]);
+void readSensors();
+void sendData();
+void showOffline();
+void getTime();
+void updateScreen();
 
 void setup() {
-  //ОБОЗНАЧЕНИЕ ПИНОВ
-  pinMode(LED_PIN, OUTPUT);
-  
-  #ifdef MQTT_DEBUG
-    MQTTClient.enableDebuggingMessages(true);
-  #endif
-
-  #ifdef BOT_DEBUG
     Serial.begin(9600);
-    Serial.print("Подключение к Wi-Fi: ");
-    Serial.print(WIFI_SSID);
-  #endif
-
-  //ПОДКЛЮЧЕНИЕ К СЕТИ
-  client.setTrustAnchors(&cert);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(2, !digitalRead(2));
-    delay(100);
-  }
-
-  
-  #ifdef BOT_DEBUG
-    Serial.print("\nWi-Fi подключен. IP: ");
-    Serial.println(WiFi.localIP());
-  #endif
-
-  //НАСТРОЙКА ВРЕМЕНИ
-  configTime(0, 0, "pool.ntp.org");
-  time_t now = time(nullptr);
-  while (now < 24 * 3600) {
-    delay(100);
-    now = time(nullptr);
-  }
-  
-  bot.setMyCommands(F("["
-                        "{\"command\":\"start\", \"description\":\"Початок роботи\"},"
-                        "{\"command\":\"reboot\",\"description\":\"Перезавантаження, на випадок відвалу MQTT-серверу\"},"
-                        "{\"command\":\"report\",\"description\":\"Поскаржитися: /report [скарга]\"}"
-                      "]"));
-
-
- 
+    screen.init();  
+    bme.begin();
+    mhz19_uart -> begin(RX_PIN, TX_PIN);
+    mhz19_uart -> setAutoCalibration(false);        
+    connectToWiFi();
+    readSensorsTimer.setInterval(5000);
+    
+    if (WiFi.status() == WL_CONNECTED) {
+        configTime(3600*UTC, 3600*UTCS, "pool.ntp.org");
+        sendDataTimer.setInterval(60000);
+        getTimeTimer.setInterval(1000);
+    }
+    screen.clear();
+    screen.fastLineH(8, 0, 128, 1);
 }
-
-
 
 void loop() {
-  MQTTClient.loop();
-  
-  //ПРОВЕРКА СООБЩЕНИЙ В БОТЕ
-  if (millis() - bot_lastmsg > BOT_MSG_DELAY) {
-
-    bot_lastmsg = millis();
-    uint8_t numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-    while (numNewMessages) {
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-      /*ПРОВЕРКА НА ФЛУД*/
-      if(numNewMessages >= 3) break;
-    }
-  }
-
-  //МИГАНИЕ СВЕТОДИОДОМ (ПО ПРИКОЛУ)
-  if (millis() - esp_lastled > LED_DELAY) {
-    esp_lastled = millis();
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-  }
-
-  //ОТПРАВКА НА MQTT-СЕРВЕР
-  if (MQTTClient.isConnected() && (millis() - mqtt_lastsend > PUB_DELAY)) {
-      mqtt_lastsend = millis();
-      MQTTClient.publish(TEMP_TOPIC, temp);
-      MQTTClient.publish(HUM_TOPIC, hum);
-      MQTTClient.publish(PRES_TOPIC, pres);
-      MQTTClient.publish(PPM_TOPIC, ppm);
-    }
-  //ОБРАБОТКА ДАТЧИКОВ
-  if (millis() - esp_lastupdate > UPDATE_DELAY) {
-    esp_lastupdate = millis();
-    temp = String(random(20, 30));
-    hum = String(random(0, 100));
-    pres = String(random(100, 300));
-    ppm = String(random(0, 700));
-  }
+    if(readSensorsTimer.isReady()) readSensors();
+    if(sendDataTimer.isReady()) sendData();
+    if(getTimeTimer.isReady()) getTime();
 }
 
-
-
-
-/*ОБРАБОТКА СООБЩЕНИЙ*/
-void handleNewMessages(int numNewMessages) {
-    #ifdef BOT_DEBUG
-    Serial.print("Новое сообщение\n");
-    #endif
-  String answer;
-  for (int i = 0; i < numNewMessages; i++) {
-    telegramMessage& msg = bot.messages[i];
+void getTime() {
+    char formattedTime[22];
+    time_t now = time(nullptr);
+    struct tm timeinfo;
+    localtime_r(&now, &timeinfo);
+    Serial.printf("Дата: %04d-%02d-%02d %02d:%02d:%02d\n",
+                timeinfo.tm_year + 1900,
+                timeinfo.tm_mon,
+                timeinfo.tm_mday,
+                timeinfo.tm_hour,
+                timeinfo.tm_min,
+                timeinfo.tm_sec);
     
-      
-     /*ПРОВЕРКА КОЛБЕК-КОМАНД НА ИХ ВАЛИДНОСТЬ*/
-     if(msg.type == "callback_query") {
-        if (msg.text == "CALLBACK_reboot") {
-          if (millis() > REBOOT_DELAY) ESP.reset();
-          if (millis() < REBOOT_DELAY) {
-            bot.sendMessage(msg.chat_id, String(FPSTR(REBOOTED_MSG)), "HTML");
-            continue;
-          } 
+    screen.setCursor(0, 0);
+    snprintf(formattedTime, sizeof(formattedTime), "%02d:%02d      %02d.%02d.%04d",
+            timeinfo.tm_hour,
+            timeinfo.tm_min,
+            timeinfo.tm_mday,
+            timeinfo.tm_mon+1,
+            timeinfo.tm_year + 1900);
+
+    screen.print(formattedTime);
+    screen.update();
+    }
+
+void connectToWiFi() {
+  WiFi.begin(WIFI_SSID , WIFI_PASSWORD);
+
+    while (WiFi.status() == WL_DISCONNECTED) {
+        Serial.println("Підключення до Wi-Fi мережі");
+        screen.clear();
+        screen.setScale(1);
+        screen.setCursor(5, 0);
+        screen.print("Пiдключення до Wi-Fi");
+        screen.update(); 
+        delay(2000);
+    }
+
+    if (WiFi.status() != WL_DISCONNECTED) {
+        switch(WiFi.status()) {
+            case WL_NO_SSID_AVAIL:
+                Serial.println("WiFi мережа з іменем " + String(WIFI_SSID) + " не знайдена");
+                showOffline();
+                break;
+            case WL_CONNECT_FAILED:
+                Serial.println("Помилка підключення до мережі");
+                showOffline();
+                break;
+            case WL_WRONG_PASSWORD:
+                Serial.println("Ви ввели неправильний пароль до мережі " + String(WIFI_SSID));
+                showOffline();
+                break;
+            case WL_CONNECTED:
+                Serial.println("\nПідключено до мережі " + String(WIFI_SSID));
+                Serial.println("Локальна IP-адреса: " + WiFi.localIP().toString());
+                Serial.println("MAC-адреса: " + WiFi.macAddress());
+                Serial.println("Адреса шлюзу: " + WiFi.gatewayIP().toString());
+                Serial.println("Адреса DNS-серверу: " + WiFi.dnsIP().toString());
+                Serial.println("Адреса шлюзу: " + WiFi.gatewayIP().toString());
+                screen.setCursor(35, 2);
+                screen.print("Пiдключено");
+                screen.update();
+                delay(1000);
+                screen.clear();
+                screen.setCursor(0, 0);
+                screen.print("IP: " + WiFi.localIP().toString());
+                screen.setCursor(0, 2);
+                screen.print("MAC:" + WiFi.macAddress());
+                screen.setCursor(0, 4);
+                screen.print("Шлюз: " + WiFi.gatewayIP().toString());
+                screen.setCursor(0, 6);
+                screen.print("DNS: " + WiFi.dnsIP().toString());
+                screen.update();
+                break;
+            default:
+                Serial.println("Невідома помилка. Повідомте розробнику");
+                showOffline();
+                break;
         }
-        if (msg.text == "continue") {
-          String keyboardJson = "[[\"Статус\", \" Перезавантажити\"]]";
-          bot.sendMessageWithReplyKeyboard(msg.chat_id, "Вітаємо в головному меню боту", "", keyboardJson, true);
-          continue;
-        }
     }
+}
 
-    /*ПРОВЕРКА НА ДЛИННУ СООБЩЕНИЯ*/
-    if (msg.text.length() > MAX_MESSAGE_LENGTH) {
-      bot.sendMessage(msg.chat_id, String(FPSTR(BIG_MSG_ERR)), "HTML");
-      bot.sendPhoto(msg.chat_id, BIG_MSG_IMG);
+void readSensors() {
+    measurement_t m = mhz19_uart -> getMeasurement();
+    humidity = (int)bme.readHumidity();
+    temperature = (int)bme.readTemperature();
+    pressure = (int)pressureToMmHg(bme.readPressure());
+    ppm = (int)m.co2_ppm;
 
-      #ifdef BOT_DEBUG
-        Serial.println("Слишком большое сообщение от " + msg.from_name + ". id: " + msg.chat_id + "\n");
-      #endif
+    screen.setCursor(0, 2);
+    screen.print("Температура: " + String(temperature));
+    screen.setCursor(0, 3);
+    screen.print("Тиск:        " + String(pressure));
+    screen.setCursor(0, 4);
+    screen.print("Вологiсть:   " + String(humidity));
+    screen.setCursor(0, 5);
+    screen.print("PPM:         " + String(ppm));
 
-      continue;
+}
+
+void showOffline() {
+    Serial.println("Запуск у офнлайн режимі");
+    screen.setScale(2);
+    screen.clear();
+    screen.setCursor(29, 3);
+    screen.print("Офлайн");
+    screen.update();
+    delay(1000);
+}
+
+void sendData() {
+    String fieldData[NUM_FIELDS]; 
+    fieldData[TEMPERATURE_FIELD] = String(temperature);
+    fieldData[HUMIDITY_FIELD]    = String(humidity);
+    fieldData[PRESSURE_FIELD]    = String(pressure);
+    fieldData[PPM_FIELD]         = String(ppm);
+    HTTPPost(NUM_FIELDS, fieldData);
+}
+
+void HTTPPost(int numFields , String fieldData[]) {
+    if (client.connect(THING_SPEAK_ADDRESS, 80)) {
+       String postData = WRITE_API_KEY;
+       for (int fieldNumber = 1; fieldNumber < numFields+1; fieldNumber++) {
+            String fieldName = "field" + String(fieldNumber);
+            postData += "&" + fieldName + "=" + fieldData[fieldNumber-1];
+            }
+
+        Serial.println("Підключення до серверу ThingSpeak...");
+        Serial.println();
+        client.println("POST /update HTTP/1.1");
+        client.println("Host: api.thingspeak.com");
+        client.println("Connection: close");
+        client.println("Content-Type: application/x-www-form-urlencoded");
+        client.println("Content-Length: " + String(postData.length()));
+        client.println();
+        client.println(postData);
+        Serial.println(postData);
+        String answer = getResponse();
+        Serial.println(answer);
     }
-
-    #ifdef BOT_DEBUG
-      Serial.println("Получено " + msg.text + " от " + msg.from_name + ". id: " + msg.chat_id + "\n");
-    #endif
-    
-    
-    /*ОБРАБОТКА КОМАНДЫ СТАРТ, /start*/
-    if (msg.text == "/start") {
-      answer = "";
-      String keyboardJson = "[[\"Статус\", \" Перезавантажити\"]]";
-          bot.sendMessageWithReplyKeyboard(msg.chat_id, "Вітаємо в головному меню боту", "", keyboardJson, true);
-    } 
-    
-    /*ОБРАБОТКА КОМАНДЫ СТАТУС, /status*/
-    else if (msg.text == "Статус" || msg.text == "/status") {
-      answer += String(FPSTR(STATUS_HEADER_STRING));
-      answer += String(FPSTR(STATUS_TEMP_STRING)) + temp;                              // + String(getTemp());
-      answer += String(FPSTR(STATUS_HUMIDITY_STRING)) + hum;                          // + String(getHumigity());
-      answer += String(FPSTR(STATUS_PPM_STRING)) + ppm;                               // + String(getPPM());
-      answer += String(FPSTR(STATUS_PRESSURE_STRING)) + pres;                          // + String(getPressure());
-      
-      answer += "\n\n";
-
-      if (MQTTClient.isMqttConnected()) answer+= String(FPSTR(STATUS_MQTT_CONNECTED));  
-      else answer+= String(FPSTR(STATUS_MQTT_DISCONNECTED));
-    } 
-    
-    /*ОБРАБОТКА КОМАНДЫ /send*/
-    else if (msg.text.startsWith("/send")) {
-      //Проверка на админа
-      if (msg.chat_id != ADMIN_ID) 
-      bot.sendMessage(msg.chat_id, String(FPSTR(YOU_ARE_NOT_ADMIN)), "HTML");
-
-      String sendCommand = msg.text.substring(6);
-      int spaceIndex = sendCommand.indexOf(' ');
-
-      if (spaceIndex != -1) {
-        String userIdStr = sendCommand.substring(0, spaceIndex);
-        String adminMsg = sendCommand.substring(spaceIndex + 1);
-
-        if (userIdStr.toInt() == 0)                                                                                           answer = String(FPSTR(INVALID_ID));
-        else if (userIdStr.toInt() != 0 && bot.sendMessage(userIdStr, String(FPSTR(MSG_FROM_ADMIN)) + adminMsg, "Markdown"))  answer = String(FPSTR(SENDED_TO_USER)) + userIdStr;
-      } 
-      else answer = String(FPSTR(INVALID_FORMAT));
-    }
-
-    /*ОБРАБОТКА КОМАНДЫ ОБРАТНОЙ СВЯЗИ /report*/
-    else if (msg.text.startsWith("/report ")) {
-      if (bot.sendMessage(ADMIN_ID, "Скарга від користувача: <b>" + msg.from_name + "</b> \nid: <b>" + String(msg.chat_id) + ".</b>\nТекст скарги: " + msg.text.substring(8), "HTML"))
-        answer = String(FPSTR(SENDED_TO_ADMIN));
-      else
-        answer = String(FPSTR(UNKNOWN_ERR));
-    }
-
-    /*ОБРАБОТКА ПЕРЕЗАГРУЗКИ*/
-    else if (msg.text == "/reboot" || msg.text == "Перезавантажити") {
-      if (msg.chat_id != ADMIN_ID) bot.sendMessage(msg.chat_id, String(FPSTR(YOU_ARE_NOT_ADMIN)), "HTML");
-      else {
-        String keyboardJson = "[[{ \"text\" : \"Так\", \"callback_data\" : \"CALLBACK_reboot\" }],[{ \"text\" : \"Hi\", \"callback_data\" : \"continue\" }]]";
-        bot.sendMessageWithInlineKeyboard(msg.chat_id, String(FPSTR(REBOOT_MSG)), "", keyboardJson);
-      }
-    } 
-    /*ОБРАБОТКА НОВЫХ КОМАНД*/
-
-
     else {
-      answer = String(FPSTR(COMMAND_ERR));
+      Serial.println ("Помилка підключення");
     }
-    if (bot.sendMessage(msg.chat_id, answer, "HTML")) answer = "";
-    else answer = String(FPSTR(UNKNOWN_ERR)); 
+}
+
+String getResponse() {
+  String response;
+  long startTime = millis();
+  delay(200);
+  while (client.available() < 1 && ((millis() - startTime) < TIMEOUT)) {
+        delay(5);
   }
+  if (client.available() > 0) {
+     char charIn;
+     do {
+         charIn = client.read();
+         response += charIn; 
+        } while (client.available() > 0);
+    }
+  client.stop();
+  return response;
 }
 
